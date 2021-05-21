b27c39c23b5862a4f05b2290a2d6e8fbe5700fd9
Kaj Syrjänen <kaj.syrjanen@tuni.fi>
2021-05-21 13:47:01 +0300

    Update uralex version information

--- versions.py
+++ versions.py
@@ -3,7 +3,11 @@
 VERSIONS                 = {1.0:
                             {"zipfile": "uralex-v1.0.zip",
                              "dir" : "lexibank-uralex-efe0a73",
-                             "url"    : "https://zenodo.org/record/1459402/files/lexibank/uralex-v1.0.zip?download=1"}}
+                             "url"    : "https://zenodo.org/record/1459402/files/lexibank/uralex-v1.0.zip?download=1"},
+                            2.0:
+                            {"zipfile": "uralex-v2.0.zip",
+                             "dir" : "lexibank-uralex-a37bb22",
+                             "url"    : "https://zenodo.org/record/4777568/files/lexibank/uralex-v2.0.zip?download=1"}}
 
 def getLatestVersion():
     '''Return latest version of uralex'''
